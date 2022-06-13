e8593ee1ad7a108802a94de53385ab4881a1143f
Thane Reynolds <thane.a.reynolds@gmail.com>
2022-06-13 14:49:38 -0700

    added inputs and some styling, also fixed broken loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './styles.css';
 import Header from './components/header/Header';
+import Meme from './components/meme/Meme'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main className="main">
+        <Meme />
+      </main>
     </div>
   );
 }
