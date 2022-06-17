66ab272b3028aaec13017edfbb2ed7c4cd5d27d3
Thane Reynolds <thane.a.reynolds@gmail.com>
2022-06-17 15:37:15 -0700

    refactored for state

--- src/components/meme/Meme.jsx
+++ src/components/meme/Meme.jsx
@@ -1,21 +1,55 @@
+import React from "react"
 import MemeData from '../../memesData'
 export default function Meme(){
+  const [meme, setMeme] = React.useState(
+    {
+      topText: "",
+      bottomText:"",
+      randomImage:"http://i.imgflip.com/1bij.jpg",
+    }
+  );
+  const [allMemeImages, setAllMemeImages] = React.useState(MemeData)
+  
   function randomMeme(){
-    const memesArray = MemeData.data.memes
-    const randomNum = Math.floor(Math.random()* MemeData.data.memes.length)
-    const url = memesArray[randomNum].url
-    console.log(url)
+    const memesArray = allMemeImages.data.memes;
+    const randomNum = Math.floor(Math.random()* allMemeImages.data.memes.length);
+    setMeme(prevState =>{
+      return{
+        ...prevState,
+        randomImage: memesArray[randomNum].url,
+      }
+    });
+  }
+  function updateTopText(e){
+    setMeme(prevState => {
+      return{
+        ...prevState,
+        topText: e.target.value
+      }
+    })
+    console.log(meme)
+  }
+  function updateBottomText(e){
+    setMeme(prevState => {
+      return{
+        ...prevState,
+        bottomText: e.target.value
+      }
+    })
+    console.log(meme)
   }
   return(
     <section className="meme">
       <div className="meme--form">
         <div className="meme--input-holder">
-          <input type="text" placeholder="top text"></input>
-          <input type="text" placeholder="bottom text"></input>
+          <input onInput={updateTopText} type="text" placeholder="top text" />
+          <input onInput={updateBottomText} type="text" placeholder="bottom text" />
         </div>
         <button onClick={randomMeme} className="meme--button">Get a new meme image</button>
+      </div>
+      <div className="meme--image-holder">
+        <img src={meme.randomImage} alt="A random meme" />
       </div>
     </section>
   )
 }
-console.log(MemeData.data.memes.length)
